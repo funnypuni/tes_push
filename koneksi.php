342610aa03226101ad50feb9f5ebf617a1880716
funnypuni <vunnyvivo@gmail.com>
2026-06-12 02:04:56 +0700

    testing

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "latihan";
+$db = "funny_latihan_kelas_kopi";
 $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
  die("Koneksi database gagal: " . mysqli_connect_error());
